4903d84017c3905bfebca654b65ca73c4fac091c
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-04-19 17:01:09 +0100

    User modal update & Projects modal
    I have updated the user modal to make it easier to fetch a users projects through a has many relationship.
    
    Added the projects model which also includes a belongs to user modal to fetch the user who owns it.

---
+++ app/Models/Projects.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Projects extends Model
+{
+    use HasFactory;
+
+    protected $table = 'projects';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+        'phase',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
