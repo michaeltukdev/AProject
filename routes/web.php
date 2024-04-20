390bd474e2e14f14cd8a64c676dafe811545bd37
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-04-20 11:21:22 +0100

    Removal of routes
    Removing the useless projects create & edit route and also the view project as we are using a modal.

--- routes/web.php
+++ routes/web.php
@@ -5,30 +5,18 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// List all projects
-
 Route::view('/', 'home')->name('home');
-Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('project');
-
-// User routes
-
-Route::middleware('auth')->group(function() {
-    Route::get('/projects/create', [ProjectController::class, 'create'])->name('create');
-    Route::get('/projects/edit', [ProjectController::class, 'edit'])->name('edit');
-});
-
-//  Auth routes
 
 Route::middleware('guest')->group(function() {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+    Route::get('/logout', function(Request $request) {
+
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+    
+        return redirect('/');
+    })->name('logout');
 });
-
-Route::get('/logout', function(Request $request) {
-
-    Auth::logout();
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
-
-    return redirect('/');
-})->name('logout');
