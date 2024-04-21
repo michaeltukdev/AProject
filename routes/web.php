ede7440d0c940f2e1381b8a360e21c651916b1c2
Michael T. <66504185+michaeltukdev@users.noreply.github.com>
2024-04-21 03:30:51 +0100

    Why did I even do this?

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,16 @@ use App\Http\Controllers\AuthController;
 
 Route::view('/', 'home')->name('home');
 
-Route::middleware('guest')->group(function() {
+Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::get('/register', [AuthController::class, 'register'])->name('register');
-
-    Route::get('/logout', function(Request $request) {
-
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-    
-        return redirect('/');
-    })->name('logout');
 });
+
+Route::get('/logout', function (Request $request) {
+
+    Auth::logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+
+    return redirect('/');
+})->name('logout');
